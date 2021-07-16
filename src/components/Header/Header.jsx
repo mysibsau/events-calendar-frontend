eb95318e9d48dc52199307684cc37cb18f49d16c
Nikita Khramushin <beachsidebearhi@gmail.com>
2021-07-16 11:25:45 +0700

    Added username to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,11 +13,18 @@ export default function Header() {
         window.location.reload();
     }
 
+    const name = localStorage.getItem('name');
+
     return(
         <div className={'header-container'}>
             <h3>Календарь событий СибГУ</h3>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+             <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', marginRight: 20, fontFamily: 'Oswald', fontSize: 20}}>
+             Привет, {name}
+             </div>
             <div className={'button-container'}>
             <Button onClick={() => logout()} type={'button'} className={'button'} variant={'contained'}>Выйти</Button>
+            </div>
             </div>
         </div>
     )
