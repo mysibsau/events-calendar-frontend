da6bd1ee68364da00b801556b950c23025bbcc20
Nikita Khramushin <beachsidebearhi@gmail.com>
2021-07-17 17:18:58 +0700

    Edited event strips

--- src/components/CalendarPage/Calendar/Calendar.jsx
+++ src/components/CalendarPage/Calendar/Calendar.jsx
@@ -3,11 +3,22 @@ import Days from './Days/Days'
 import Months from './Months/Months';
 import Years from './Years/Years';
 import './Calendar.scss';
+import { getEventsByDate } from '../../../api/events';
 
 
 export default function Calendar() {
   const [month, setMonth] = React.useState(new Date().getMonth());
   const [year, setYear] = React.useState(new Date().getFullYear());
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    getEvents();
+  }, [month, year])
+
+  const getEvents = async () => {
+    const response = await getEventsByDate(String(month + 1).length === 1 ? '0' + (month+1) : month + 1, year);
+    setEvents(response)
+  };
     
     return(
       <div style={{width: '70vw', alignSelf: 'center'}}>
@@ -17,7 +28,7 @@ export default function Calendar() {
             console.log(y)
             setYear(y)}}/>
         </div>
-        <Days month={month} year={year}/>
+        <Days month={month} year={year} events={events}/>
       </div>
     )
 }
--- src/components/CalendarPage/Calendar/Days/Days.jsx
+++ src/components/CalendarPage/Calendar/Days/Days.jsx
@@ -1,178 +1,150 @@
 import React from 'react';
-import { getEventsByDate } from '../../../../api/events';
-import './Days.scss';
+import './Days.scss'
 
 const weekdayshort = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 
 
-export default function Days({month, year, onEventSelect}) {
-    const [daysInMonth, setDaysInMonth] = React.useState(null)
-    const [events, setEvents] = React.useState([]);
+export default function Days({events, month, year}){
+    const [weeks, setWeeks] = React.useState([])
 
     React.useEffect(() => {
-      getEvents();
-    }, [month, year])
-
-    const getEvents = async() => {
-      const events = await getEventsByDate(correctMonth(), year);
-      console.log(events)
-      setEvents(events)
-      generateDays(events);
-    }
-
-    const correctMonth = () => {
-      let res = String(month + 1);
-      if(res.length === 1){
-        res = '0' + res;
-      }
-      
-      return res;
-    }
-
-    function hashCode(str) { // java String#hashCode
-      var hash = 0;
-      for (var i = 0; i < str.length; i++) {
-         hash = str.charCodeAt(i) + ((hash << 3) - hash);
-      }
-      return hash;
-    } 
-  
-    function intToRGB(i){
-        var c = (i & 0x00FFFFFF)
-            .toString(16)
-            .toUpperCase();
-    
-        return "00000".substring(0, 6 - c.length) + c;
-    }
+        console.log(events)
+        generateDays();
+    }, [])
 
     const firstDayOfMonth = () => {
         return new Date(year, month, 1).getDay();
     };
 
-    const daysCount =  (month, year) =>  {
+    const daysCount = () =>  {
         return new Date(year, month, 0).getDate();
     }
 
-    const beginMargin = (events, i, d) => {
-      let count = 0;
-      for(let j = 0; j < i; j++){
-        if (Number(events[j].start_date.split('-')[2]) > Number(events[i].start_date.split('-')[2]) && 
-        Number(events[j].start_date.split('-')[2]) <= Number(events[i].stop_date.split('-')[2])){
-          count ++;
-        }
-      }
+    const getWeekEventsCount = (week) => {
+        let eventsCount = 0;
+        events.map(item => {
+            let start = new Date(`${item.start_date}T00:00:00`)
+            let stop = new Date(`${item.stop_date}T00:00:00`)
+            if ((week.start_date > start &&
+                week.start_date < stop) || 
+                (week.stop_date > start && week.stop_date < stop) ||
+                (start >= week.start_date && stop <= week.stop_date)){
+                eventsCount++;
+            }
+        })
 
-      return count * 45;
-    } 
-
-    const endMargin = (events, i, d) => {
-      let count = 0;
-      for (let j = 0; j < i; j++){
-        if (Number(events[j].stop_date.split('-')[2]) >= Number(events[i].start_date.split('-')[2]) && 
-        Number(events[j].stop_date.split('-')[2]) < Number(events[i].stop_date.split('-')[2])) {
-          count ++
-        }
-      }
-
-      return count * 45
+        return eventsCount;
     }
 
-    const middleMargin = (events, i, d) => {
-      let count = 0;
-      let eventsCount = 0;
-      for (let j = 0; j < i; j++){
-        if (
-          (Number(events[j].stop_date.split('-')[2]) < Number(events[i].stop_date.split('-')[2]) &&
-          Number(events[i].start_date.split('-')[2] <= events[j].stop_date.split('-')[2])) ||
-
-          ((Number(events[j].start_date.split('-')[2]) > Number(events[i].start_date.split('-')[2]) &&
-          Number(events[i].stop_date.split('-')[2]) >= Number(events[j].start_date.split('-')[2])) &&
-          (d < Number(events[j].start_date.split('-')[2]) || d > Number(events[j].stop_date.split('-')[2])) )
-        )
-        count ++;
-
-        if (d < Number(events[j].start_date.split('-')[2]) || d > Number(events[j].stop_date.split('-')[2])){
-          console.log(d)
-          eventsCount++;
-        }
-      }
-
-      console.log(eventsCount, d, count)
-      if (eventsCount === count && i !== 0) count --;
-      // count += Math.abs(count - someCount)
-
-      return count * 45;
-    }
-
-    const generateDays = (events) => {
-        let blanks = [];
-        for (let i = 0; i < firstDayOfMonth() - 1; i++) {
-            blanks.push(
-                <td className="calendar-day empty">{""}</td>
-            );
-        }
-    
-        let daysInMonth = [];
-        for (let d = 1; d <= daysCount(new Date().getMonth(), new Date().getFullYear()); d++) {
-          daysInMonth.push(
-              <td key={d} className="calendar-day" valign='top'>
-              {d}
-              {events.map(item => {
-                if (Number(item.start_date.split('-')[2]) === d && Number(item.stop_date.split('-')[2]) === d){
-                  return (<div onClick={() => onEventSelect(item)} style={{width: '10vw', height: 30,borderRadius: 15, whiteSpace: 'nowrap', color: 'white', backgroundColor: '#' +  intToRGB(hashCode(item.name)),  padding: 5, marginTop: 5}}>{item.name}</div>)
-                } else if(Number(item.start_date.split('-')[2]) === d){
-                  return <div style={{width: '10vw', height: 30,padding: 5, whiteSpace: 'nowrap', borderTopLeftRadius: 15, color: 'white', backgroundColor: '#' +  intToRGB(hashCode(item.name)), borderBottomLeftRadius: 15,  borderRightWidth: 0, marginTop: beginMargin(events, events.indexOf(item), d) + 5}}>{item.name}</div>
-                } else if(Number(item.stop_date.split('-')[2]) === d) {
-                  return <div style={{width: '10vw',height: 30,borderTopRightRadius: 15, borderBottomRightRadius: 15, color: 'white', backgroundColor: '#' +  intToRGB(hashCode(item.name)), borderLeftWidth: 0, padding: 5, marginTop: endMargin(events, events.indexOf(item), d) + 5}}></div>
-                } else if(Number(item.start_date.split('-')[2]) < d && Number(item.stop_date.split('-')[2]) > d){
-                  return <div style={{width: '10vw',height: 30, color: 'white', backgroundColor: '#' +  intToRGB(hashCode(item.name)), borderLeftWidth: 0, borderRightWidth: 0, padding: 5, marginTop: middleMargin(events, events.indexOf(item), d) + 5}}></div>
-                }
-              })}
-              </td>
-          );
-        }
-
-        var totalSlots = [...blanks, ...daysInMonth];
-        let rows = [];
-        let cells = [];
-
-        totalSlots.forEach((row, i) => {
-            if (i % 7 !== 0) {
-              cells.push(row);
-            } else {
-              rows.push(cells);
-              cells = []; 
-              cells.push(row); 
-            }
-            if (i === totalSlots.length - 1) { 
-              rows.push(cells);
-            }
-          });
+    const generateDays = () => {
+        let weeks = [];
+        let weekList = [];
+        let week = {};
         
-        let daysinmonth = rows.map((d, i) => {
-          return <tr>{d}</tr>;
-        });
+        for (let i = 0; i < firstDayOfMonth() - 1; i++){
+            weekList.push(null)
+        }
 
-        setDaysInMonth(daysinmonth);
+        for (let i = 0; i <= daysCount(); i++){
+            weekList.push(new Date(`${year}-${String(month + 1).length === 1 ? '0' + (month + 1) : month + 1}-${String(i + 1).length === 1 ? '0' + (i + 1) : i + 1}T00:00:00`));
+            if (weekList.length === 7){
+                for (let j = 0; j < 7; j++){
+                    if (weekList[j] !== null){
+                        week['start_date'] = weekList[j];
+                        break
+                    }
+                }
+                week['stop_date'] = weekList[weekList.length - 1];
+                week['list'] = weekList;
+                weeks.push(week)
+                weekList = [];
+                week = {}
+            }
+        }
+
+        for (let j = 0; j < weekList.length; j++){
+            if (weekList[j] !== null){
+                week['start_date'] = weekList[j];
+                break
+            }
+        }
+        week['stop_date'] = weekList[weekList.length - 1];
+        week['list'] = weekList;
+        weeks.push(week)
+
+        setWeeks(weeks);
     }
 
+    function hashCode(str) { // java String#hashCode
+        var hash = 0;
+        for (var i = 0; i < str.length; i++) {
+           hash = str.charCodeAt(i) + ((hash << 3) - hash);
+        }
+        return hash;
+      } 
+    
+      function intToRGB(i){
+          var c = (i & 0x00FFFFFF)
+              .toString(16)
+              .toUpperCase();
+      
+          return "00000".substring(0, 6 - c.length) + c;
+      }
 
     return(
-        <table className="calendar-div">
-        <thead className="weekday-head">
+        <table>
+          <thead className="weekday-head">
             <tr>
-          {weekdayshort.map(day => {
-            return(
-              <th key={day} className="week-day">
-                {day}
-              </th>
-            )
-          })}
-          </tr>
-        </thead>
-        <tbody>
-            {daysInMonth}
-        </tbody>
-      </table>
-    );
+            {weekdayshort.map(day => {
+                return(
+                <th key={day} className="week-day">
+                    {day}
+                </th>
+                )
+            })}
+            </tr>
+          </thead>
+          <tbody>
+            {weeks.map(item => {
+                let eventsCount = getWeekEventsCount(item)
+                let weekEvents = events.filter(event => {
+                    let start = new Date(`${event.start_date}T00:00:00`)
+                    let stop = new Date(`${event.stop_date}T00:00:00`)
+                    if ((item.start_date > start &&
+                        item.start_date < stop) || 
+                        (item.stop_date > start && item.stop_date < stop) ||
+                        (start >= item.start_date && stop <= item.stop_date)){
+                        return event
+                    }
+                })
+                console.log(weekEvents)
+                return(
+                    <tr style={{height: (eventsCount <= 1 ? 100 : eventsCount * 50 + 20)}}>
+                        {item.list.map(d => {
+                            if(d)
+                                return (<td valign='top' className="calendar-day" style={{overflow: 'visible'}}>
+                                    {d.getDate()}
+                                    {weekEvents.map(event => {
+                                        let start = new Date(`${event.start_date}T00:00:00`)
+                                        let stop = new Date(`${event.stop_date}T00:00:00`)
+                                        if (d.valueOf() === start.valueOf() && d.valueOf() === stop.valueOf()){
+                                            return (<div className="event-div" style={{ borderRadius: 15, backgroundColor: '#' +  intToRGB(hashCode(event.name)), padding: 5, marginTop: weekEvents.indexOf(event) * 45 + 5}}>{event.name}</div>)
+                                          } else if(d.valueOf() === start.valueOf()){
+                                            return <div className="event-div" style={{ borderTopLeftRadius: 15, zIndex: 3, backgroundColor: '#' +  intToRGB(hashCode(event.name)), borderBottomLeftRadius: 15, marginTop: weekEvents.indexOf(event) * 45 + 5}}>{event.name}</div>
+                                          } else if(d.valueOf() === stop.valueOf()) {
+                                            return <div className="event-div" style={{borderTopRightRadius: 15, borderBottomRightRadius: 15, backgroundColor: '#' +  intToRGB(hashCode(event.name)), marginTop: weekEvents.indexOf(event) * 45 + 5}}></div>
+                                          } else if(d.valueOf() > start.valueOf() && stop.valueOf() > d.valueOf() ){
+                                            return <div className="event-div" style={{backgroundColor: '#' +  intToRGB(hashCode(event.name)), marginTop: weekEvents.indexOf(event) * 45 + 5}}></div>
+                                          }
+                                    })}
+                                    </td>)
+                            else 
+                                return <td className="calendar-day empty"/>
+                        })}
+                    </tr>
+                )
+            })}
+          </tbody>
+        </table>
+    )
 }
